a21d9cc3b79ed5588dc3009a9c0493e3b8260714
José García Borge <adrian23gb@hotmail.com>
2021-07-05 20:59:36 -0600

    Responsive del Buscar, Agregacion del Reparto en detalles de la peli

--- js/nuevo.js
+++ js/nuevo.js
@@ -32,12 +32,40 @@ media = geturl.get('media');
         // always executed
       });
 
+
+      axios.get('https://api.themoviedb.org/3/tv/'+id+'/credits?api_key=c94ae96d76db457ccdb3767fef477a02&language=es-US')
+      .then(function(response){
+         
+        let carrusel = "";
+        var reparto = document.querySelector("#autoWidth");
+        response.data.cast.forEach(element =>{
+              carrusel += "<li class=\"item-a\">"
+              +"<div class=\"contenedor\">"
+                  +"<img class=\"persona\" src=\"https://www.themoviedb.org/t/p/w138_and_h175_face/"+element.profile_path+"\">"
+                  +"<h4 class=\"NombreReal\">"+element.name+"</h4>"
+                  +"<h5 class=\"NombrePersonaje\">"+element.character+"</h5>"
+              +"</div>"  
+          +"</li>"
+        });
+        reparto.innerHTML = carrusel;
+
+
+        console.log(response);
+      }).catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+      
+
       }else{
 
         axios.get('https://api.themoviedb.org/3/movie/'+id+'?api_key=c94ae96d76db457ccdb3767fef477a02')
       .then(function (response) {
         // handle success
-        console.log(response.data);
+        
         document.querySelector("#titulo").innerHTML = "<h2>"+response.data.original_title+"</h2>";
         document.querySelector(".posteroriginal").src = "https://image.tmdb.org/t/p/w500"+response.data.poster_path;
         document.querySelector(".header_pelis").style.background = "url('https://image.tmdb.org/t/p/original/"+response.data.backdrop_path+"') top center no-repeat no-repeat";
@@ -66,5 +94,32 @@ media = geturl.get('media');
         // always executed
       });
 
+
+      axios.get('https://api.themoviedb.org/3/movie/'+id+'/credits?api_key=c94ae96d76db457ccdb3767fef477a02&language=es-US ')
+      .then(function(response){
+        
+        let carrusel = "";
+        var reparto = document.querySelector("#autoWidth");
+        response.data.cast.forEach(element =>{
+              carrusel += "<li class=\"item-a\">"
+              +"<div class=\"contenedor\">"
+                  +"<img class=\"persona\" src=\"https://www.themoviedb.org/t/p/w138_and_h175_face/"+element.profile_path+"\">"
+                  +"<h4 class=\"NombreReal\">"+element.name+"</h4>"
+                  +"<h5 class=\"NombrePersonaje\">"+element.character+"</h5>"
+              +"</div>"  
+          +"</li>"
+        });
+        reparto.innerHTML = carrusel;
+
+      }).catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+      
+
       }
+
 
--- js/sliderAPI.js
+++ js/sliderAPI.js
@@ -1,19 +1,18 @@
 axios.get('https://api.themoviedb.org/3/tv/popular?api_key=c94ae96d76db457ccdb3767fef477a02&language=es-US&page=1')
 .then(function(response){
-    console.log(response);
-  
+
     var populares = response.data.results;
     let template = "";
 
   populares.forEach(element => {
       template += 
 
-         "<li class=\"item-Ea\">"+
+         "<li class=\"item-a\">"+
              "<div class=\"showcase-box\">"+
                  "<img src=\"https://image.tmdb.org/t/p/w500"+element.poster_path+"\">"+
                  "<div class=\"capa\">"+
                      "<h3>"+element.original_name+"</h3>"+
-                     "<p>sidhasjdhaosidjapsofja[doskf[aspkd[adpfjapdf</p>"+
+                     "<p>"+element.overview+"</p>"+
                  "</div>"+
              "</div>"+
          "</li>";
