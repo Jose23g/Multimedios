5307ef032b9f917b6a9b9bd4d74ca48570a8ee59
Josue Machado Velasquez <josue-1231@hotmail.es>
2021-07-15 23:01:38 -0600

    Funcion de los iconos

--- js/opciones.js
+++ js/opciones.js
@@ -11,9 +11,33 @@ var verseries = document.querySelector('.series');
 
        function estrenos() {
         axios.get('https://api.themoviedb.org/3/movie/upcoming?language=es-es&page=1&api_key=c94ae96d76db457ccdb3767fef477a02')
-        .then(function(response) {
+        .then(function(response) { 
           let contenido = "";
           contenido +="<h2>Estrenos</h2>";
+          var peli =  response.data.results; 
+          peli.forEach(element => {
+            contenido += 
+            "<div class=\"pelis\">"+
+                 "<a onclick=\"eligiendo()\" href=\"detallePeli.html?id="+element.id+"&media="+element.media_type+"\">"+
+                     "<div class=\"imagen\">"+
+                         "<img src=\"https://image.tmdb.org/t/p/w500"+element.poster_path+"\" class=\"cartelera\">"+
+                     "</div>"+
+                     "<div class=\"circulo\">"+
+                         "<label>"+element.vote_average+"</label>"+
+                     "</div>"+
+                     "<div class=\"info-peli\">"+
+                         "<p class=\"titulo flex flex-col\">"+element.original_title+"<samp class=\"categoria\">Pelicula</samp></p> "+
+                         "<div>"+
+                             "<input type=\"button\" value=\"100m \" class=\"btn-dura\">"+
+                         "</div>"+
+                         "<div class=\"peli-descripcion\">"+
+                             "<p>"+element.overview+"</p>"+
+                         "</div>"+
+                     "</div>"+
+                 "</a>"+
+             "</div>";
+
+    });
           pintaropcionesmenu.innerHTML = contenido;
         }).catch(function (error) {
           // handle error
@@ -25,10 +49,34 @@ var verseries = document.querySelector('.series');
        }
 
        function populares(){
-        axios.get('https://api.themoviedb.org/3/movie/upcoming?language=es-es&page=1&api_key=c94ae96d76db457ccdb3767fef477a02')
+        axios.get('https://api.themoviedb.org/3/trending/all/week?api_key=c94ae96d76db457ccdb3767fef477a02')
         .then(function(response) {
           let contenido = "";
-          contenido +="<h2>Populares</h2>";
+          contenido +="<h2>Populares</h2>"; 
+          var peli =  response.data.results; 
+          peli.forEach(element => {
+            contenido += 
+            "<div class=\"pelis\">"+
+                 "<a onclick=\"eligiendo()\" href=\"detallePeli.html?id="+element.id+"&media="+element.media_type+"\">"+
+                     "<div class=\"imagen\">"+
+                         "<img src=\"https://image.tmdb.org/t/p/w500"+element.poster_path+"\" class=\"cartelera\">"+
+                     "</div>"+
+                     "<div class=\"circulo\">"+
+                         "<label>"+element.vote_average+"</label>"+
+                     "</div>"+
+                     "<div class=\"info-peli\">"+
+                         "<p class=\"titulo flex flex-col\">"+element.original_title+"<samp class=\"categoria\">"+element.media_type+"</samp></p> "+
+                         "<div>"+
+                             "<input type=\"button\" value=\"100m \" class=\"btn-dura\">"+
+                         "</div>"+
+                         "<div class=\"peli-descripcion\">"+
+                             "<p>"+element.overview+"</p>"+
+                         "</div>"+
+                     "</div>"+
+                 "</a>"+
+             "</div>";
+
+          });
           pintaropcionesmenu.innerHTML = contenido;
         }).catch(function (error) {
           // handle error
@@ -41,11 +89,38 @@ var verseries = document.querySelector('.series');
       
 
        function series(){
-        axios.get('https://api.themoviedb.org/3/movie/upcoming?language=es-es&page=1&api_key=c94ae96d76db457ccdb3767fef477a02')
-        .then(function(response) {
+        axios.get('https://api.themoviedb.org/3/tv/popular?api_key=c94ae96d76db457ccdb3767fef477a02&idioma=es-es&página=1')
+        .then(function(response) { 
           let contenido = "";
+          var peli =  response.data.results; 
           contenido +="<h2>SERIES</h2>";
-          pintaropcionesmenu.innerHTML = contenido;
+          peli.forEach(element => { 
+
+            contenido += 
+            "<div class=\"pelis\">"+
+                 "<a href=\"detallePeli.html?id="+element.id+"&media="+element.media_type+"\">"+
+                     "<div class=\"imagen\">"+
+                         "<img src=\"https://image.tmdb.org/t/p/w500"+element.poster_path+"\" class=\"cartelera\">"+
+                     "</div>"+
+                     "<div class=\"circulo\">"+
+                         "<label>"+element.vote_average+"</label>"+
+                     "</div>"+
+                     "<div class=\"info-peli\">"+
+                         "<p class=\"titulo flex flex-col\">"+element.name+"<samp class=\"categoria\">Series</samp></p> "+
+                         "<div>"+
+                             "<input type=\"button\" value=\"100m \" class=\"btn-dura\">"+
+                         "</div>"+
+                         "<div class=\"peli-descripcion\">"+
+                             "<p>"+element.overview+"</p>"+
+                         "</div>"+
+                     "</div>"+
+                 "</a>"+
+             "</div>";
+          });
+
+          pintaropcionesmenu.innerHTML = contenido; 
+
+
         }).catch(function (error) {
           // handle error
           console.log(error);
@@ -56,7 +131,7 @@ var verseries = document.querySelector('.series');
        }
 
        function movies () {
-        axios.get('https://api.themoviedb.org/3/movie/popular?api_key=c94ae96d76db457ccdb3767fef477a02&language=en-US&page=1')
+        axios.get('https://api.themoviedb.org/3/movie/popular?api_key=c94ae96d76db457ccdb3767fef477a02&language=es-es&page=1')
         .then(function (response) {
             let contenido = "";
             contenido += "<h2>Peliculas</h2>"
@@ -74,7 +149,7 @@ var verseries = document.querySelector('.series');
                                  "<label>"+element.vote_average+"</label>"+
                              "</div>"+
                              "<div class=\"info-peli\">"+
-                                 "<p class=\"titulo flex flex-col\">"+element.original_title+"<samp class=\"categoria\">"+element.media_type+"</samp></p> "+
+                                 "<p class=\"titulo flex flex-col\">"+element.original_title+"<samp class=\"categoria\">Pelicula</samp></p> "+
                                  "<div>"+
                                      "<input type=\"button\" value=\"100m \" class=\"btn-dura\">"+
                                  "</div>"+
